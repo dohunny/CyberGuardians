bbf40e2702d6e891797ac85989d8716ff99a99e8
dohunny <dhkoo.hacker@gmail.com>
2021-10-19 13:21:52 +0000

    ls -al 구현

--- lsh_ls.c
+++ lsh_ls.c
@@ -40,78 +40,86 @@ void rwx(file_mode){
 
 
 int lsh_ls(char **args){
-  DIR* dp = NULL;
-  struct dirent* entry = NULL;
-  struct stat file_info;
-  int option_exist; int longest;
-  char detail[50] = { 0x00, };
-  char format[50] = { 0x00, };
-  char buf1[80];
-	struct passwd *my_passwd;
-  struct group *my_group;
-  struct tm *tminfo;
+    DIR* dp = NULL;
+    struct dirent* entry = NULL;
+    struct stat file_info;
+    int option_exist; int longest; int allOption = 0;
+    char detail[50] = { 0x00, };
+    char format[50] = { 0x00, };
+    char buf1[80];
+    struct passwd *my_passwd;
+    struct group *my_group;
+    struct tm *tminfo;
 
-  if(args[1] == NULL){
-    // Path X, Option X
-    if ((dp=opendir("./")) == NULL) {
-      printf("Not Found: %s\n", "./");
-      return -1;
-    }
-    option_exist = 0;
-  } else if(args[1][0] != '-') {
-    // Path O, Option X
-    if ((dp=opendir(args[1])) == NULL) {
-      printf("Not Found: %s\n", args[1]);
-      return -1;
-    }
-    option_exist = 0;
-  } else {
-    option_exist = 1;
-    if (args[2] == NULL) {
-    // Path X, Option O
-      if ((dp=opendir("./")) == NULL) {
-        printf("Not Found: %s\n", "./");
-        return -1;
-      }
-    }else{
-      // Path O, Option O
-      if ((dp=opendir(args[2])) == NULL) {
-        printf("Not Found: %s\n", args[2]);
-        return -1;
-      }
-    }
-  }
-
-  while ((entry = readdir(dp)) != NULL) {
-    lstat(entry->d_name, &file_info);
-    longest = 5;
-    if (option_exist){
-      if(entry->d_name[0] == 0x2E) continue;
-      
-      rwx(file_info.st_mode);
-      printf("%ld  ", file_info.st_nlink);
-
-      my_passwd = getpwuid(file_info.st_uid);
-      my_group  = getgrgid(file_info.st_gid);
-      printf("%s  ", my_passwd->pw_name);
-      printf("%s  ", my_group->gr_name);
-      printf("%ld  ", file_info.st_size);
-
-      tminfo = localtime(&(file_info.st_atime));
-      strftime(buf1, 80, "%m-%d %H:%M", tminfo); 
-      printf("%s  ", buf1); 
-
-      if(entry->d_ino != 0) 
-      printf("%s\n", entry->d_name);
-
+    if(args[1] == NULL){
+        // Path X, Option X
+        if ((dp=opendir("./")) == NULL) {
+            printf("Not Found: %s\n", "./");
+            return -1;
+        }
+        option_exist = 0;
+    } else if(args[1][0] != '-') {
+        // Path O, Option X
+        if ((dp=opendir(args[1])) == NULL) {
+            printf("Not Found: %s\n", args[1]);
+            return -1;
+        }
+        option_exist = 0;
     } else {
-      if(entry->d_name[0] == 0x2E) continue;
-      printf("%s  ", entry->d_name);
+        if (args[2] == NULL) {
+            // Path X, Option O
+            if ((dp=opendir("./")) == NULL) {
+                printf("Not Found: %s\n", "./");
+                return -1;
+            }
+            option_exist = 1;
+        }else{
+            // Path O, Option O
+            if ((dp=opendir(args[2])) == NULL) {
+                printf("Not Found: %s\n", args[2]);
+                return -1;
+            }
+            option_exist = 1;
+        }
     }
-  }
 
-  printf("\n");
-  closedir(dp);
+    if (option_exist){
+        for(int i = 0; i < strlen(args[option_exist]); i++){
+            if (args[option_exist][i] == 'a') {
+                allOption = 1;
+                break;
+            }
+        }
+    }
 
-  return 1;
+    while ((entry = readdir(dp)) != NULL) {
+        lstat(entry->d_name, &file_info);
+        longest = 5;
+        if (option_exist){
+            if(entry->d_name[0] == 0x2E && !allOption) continue;
+
+            rwx(file_info.st_mode);
+            printf("%ld  ", file_info.st_nlink);
+
+            my_passwd = getpwuid(file_info.st_uid);
+            my_group  = getgrgid(file_info.st_gid);
+            printf("%s  ", my_passwd->pw_name);
+            printf("%s  ", my_group->gr_name);
+            printf("%ld  ", file_info.st_size);
+
+            tminfo = localtime(&(file_info.st_atime));
+            strftime(buf1, 80, "%m-%d %H:%M", tminfo); 
+            printf("%s  ", buf1); 
+
+            if(entry->d_ino != 0) printf("%s\n", entry->d_name);
+        } else {
+            if(entry->d_name[0] == 0x2E) continue;
+            printf("%s  ", entry->d_name);
+        }
+    }
+
+    printf("\n");
+    closedir(dp);
+
+    return 1;
 }
