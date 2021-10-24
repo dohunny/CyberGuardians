d5ed6822535015ef722d0091611a9041b57662a1
dohunny <dhkoo.hacker@gmail.com>
2021-10-24 02:04:57 +0000

    ls 파일 종류 출력 수정

--- SimpleShell/lsh_ls.c
+++ SimpleShell/lsh_ls.c
@@ -4,37 +4,63 @@
 
 void rwx(file_mode){
 
-  if (S_ISDIR(file_mode)) printf("d");
-  else printf("-");
+    int kind;
 
-  if (file_mode & S_IRUSR) printf("r");
-  else printf("-");
+    kind = file_mode & S_IFMT; 
+    switch (kind) { 
+        case S_IFIFO: 
+            printf("p");
+            break; 
+        case S_IFCHR:
+            printf("c");
+            break;
+        case S_IFDIR: 
+            printf("d"); 
+            break;
+        case S_IFBLK:
+            printf("b");
+            break;
+        case S_IFREG: 
+            printf("-"); 
+            break; 
+        case S_IFLNK:
+            printf("l");
+            break;
+        case S_IFSOCK:
+            printf("s");
+            break;
+        default:
+            printf("-");
+    }
 
-  if (file_mode & S_IWUSR) printf("w");
-  else printf("-");
+    if (file_mode & S_IRUSR) printf("r");
+    else printf("-");
 
-  if (file_mode & S_IXUSR) printf("x");
-  else printf("-"); 
+    if (file_mode & S_IWUSR) printf("w");
+    else printf("-");
 
-  if (file_mode & S_IRGRP) printf("r");
-  else printf("-");
+    if (file_mode & S_IXUSR) printf("x");
+    else printf("-"); 
 
-  if (file_mode & S_IWGRP) printf("w");
-  else printf("-");
+    if (file_mode & S_IRGRP) printf("r");
+    else printf("-");
 
-  if(file_mode & S_IXGRP) printf("x");
-  else printf("-"); 
+    if (file_mode & S_IWGRP) printf("w");
+    else printf("-");
 
-  if (file_mode & S_IROTH) printf("r");
-  else printf("-");
+    if(file_mode & S_IXGRP) printf("x");
+    else printf("-"); 
 
-  if (file_mode & S_IWOTH) printf("w");
-  else printf("-");
+    if (file_mode & S_IROTH) printf("r");
+    else printf("-");
 
-  if(file_mode & S_IXOTH) printf("x");
-  else printf("-");
+    if (file_mode & S_IWOTH) printf("w");
+    else printf("-");
 
-  printf("  ");
+    if(file_mode & S_IXOTH) printf("x");
+    else printf("-");
+
+    printf("  ");
 }
 
 int lsh_ls(char **args){
